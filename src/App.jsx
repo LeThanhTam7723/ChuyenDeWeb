e12f666ffc278ebf25ec2045d14ebcc22d3fd9a9
LeThanhTam7723 <tamle7723@gmail.com>
2025-02-25 23:52:53 +0700

    home page
    UI homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react'
 import Navbar from './assets/components/navbar/navbar'
+import HomePage from './assets/pages/homepage/homepage'
 
 const App = () => {
-
   const [theme,setTheme] = useState('light');
 
   return (
     <div className={`container ${theme}`}>
       <Navbar theme={theme} setTheme={setTheme}/>
-      <h1>Hello</h1>
+      <HomePage/>
     </div>
+    
   )
 }
 
--- src/assets/components/navbar/navbar.jsx
+++ src/assets/components/navbar/navbar.jsx
@@ -6,6 +6,13 @@ import search_icon_light from '../../images/header/search-w.png'
 import search_icon_dark from '../../images/header/search-b.png'
 import toogle_light from '../../images/header/night.png'
 import toogle_dark from '../../images/header/day.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+
 
 const Navbar = ({theme, setTheme}) => {
   const toggle_mode = () => {
@@ -14,18 +21,30 @@ const Navbar = ({theme, setTheme}) => {
 
   return (
     <div className={`navbar ${theme}`}>
-      <img src={theme == 'light' ? logo_dark : logo_light} alt="" className='logo'></img>
-      <ul>
-        <li>Home</li>
-        <li>Product</li>
-        <li>Feature</li>
-        <li>About</li>
-      </ul>
-      <div className='search-box'>
-        <input type='text' placeholder='Search' />
-        <img src={theme =='light' ? search_icon_dark : search_icon_light}></img>
+      <div className="navbar-left">
+        <img src={theme == 'light' ? logo_dark : logo_light} alt="" className='logo'></img>
+        <ul>
+          <li>Trang chủ</li>
+          <li>Cửa hàng</li>
+          <li>Thông tin</li>
+          <li>Liên hệ</li>
+        </ul>
+        <div className='search-box'>
+          <input type='text' placeholder='Tìm kiếm' />
+          <img src={theme =='light' ? search_icon_dark : search_icon_light}></img>
+        </div>
+        <div className='space'></div>
       </div>
-      <img onClick={()=>{toggle_mode()}} src={theme =='light' ? toogle_dark : toogle_light} alt='' className='toggle-icon' />
+      
+      <div className="navbar-right">
+        <ul className='menu'>
+          <img onClick={()=>{toggle_mode()}} src={theme =='light' ? toogle_dark : toogle_light} alt='' className='toggle-icon' />
+          <li><FontAwesomeIcon icon={faCartShopping} /></li>
+          <li><FontAwesomeIcon icon={faLanguage} /></li>
+          <li><FontAwesomeIcon icon={faHeart} /></li>
+          <li><FontAwesomeIcon icon={faUser} /></li>
+        </ul>
+      </div>  
     </div>
   )
 }
